319eb6d07daff76bd6257644b544d997d91d7c14
guocz <2589945540@qq.com>
2023-10-26 10:08:59 +0800

    Material Toolbar

--- app/src/main/java/com/example/meterialtest/MainActivity.java
+++ app/src/main/java/com/example/meterialtest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.meterialtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (R.id.backup == itemId) {
+            Toast.makeText(this, "you clicked backup", Toast.LENGTH_SHORT).show();
+        } else if (R.id.delete == itemId) {
+            Toast.makeText(this, "you clicked delete", Toast.LENGTH_SHORT).show();
+        } else if (R.id.setting == itemId) {
+            Toast.makeText(this, "you clicked setting", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+        return true;
     }
 }
